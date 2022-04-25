3d06ca31b779ef0e9e2bd8d80b4e4c35eaaeb5ff
brunosoharagit <b.sohara.git@gmail.com>
2022-04-25 10:01:01 -0300

    OBC 2022.04.25

---
+++ Javascript/modulo01/primitivetyles.js
@@ -0,0 +1,16 @@
+var numero = 4;
+var letra = 'a';
+var nome = "Bruno";
+var moeda = 4.50;
+var objeto = {
+    nome: 'Bruno',
+    idade: 25
+}
+var lista = ['abacaxi', 'pera', 'morango'];
+
+console.log(numero);
+console.log(letra);
+console.log(nome);
+console.log(moeda);
+console.log(objeto.idade);
+console.log(lista[1]);
