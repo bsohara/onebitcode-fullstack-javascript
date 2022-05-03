8ae7ecb0000a2092876595068ae8710ea2855a91
b.sohara.git <b.sohara.git@gmail.com>
2022-05-02 21:49:28 -0300

    OBC 2022.05.02

---
+++ Javascript/revisao/script.js
@@ -0,0 +1,27 @@
+frontFrame = ['ReactJS', 'VueJS', 'NextJS'];
+backFrame = ['NodeJS', 'BackboneJS', 'Express'];
+mobiFrame = ['Ionic', 'React Native', 'Meteor'];
+deskFrame = ['Electron', 'Meteor Desk', 'NodeGUI'];
+
+var option = parseInt(prompt(`Qual stack utilizar?
+                            \n1 - frontend
+                            \n2 - backend
+                            \n3 - mobile
+                            \n4 - desktop`));
+
+switch (option) {
+  case 1:
+    break;
+
+  case 2:
+    break;
+
+  case 3:
+    break;
+
+  case 4:
+    break;
+
+  default:
+}
+
