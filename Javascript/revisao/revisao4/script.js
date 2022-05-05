e30650c8681309de7974700467a53d2bc167785a
brunosoharagit <b.sohara.git@gmail.com>
2022-05-05 10:47:59 -0300

    OBC 2022.05.05 - arrays revisao

--- Javascript/revisao/revisao4/script.js
+++ Javascript/revisao/revisao4/script.js
@@ -1,14 +1,44 @@
 let nomes = [];
-let opcao = prompt(`O que você quer fazer?
-                1 - adicionar um nome;
-                2 - remover um nome;
-                3 - encontrar a posição deste nome;
-                4 - listar os nomes;
-                5 - monstrar número de nomes
-                6 - sair do programa`);
-
 let nome = prompt('Digite um nome: ');
+let opcao = parseInt(prompt(`Escolha uma opção a ser realizada:
+1 - Adicionar um nome
+2 - Encontrar um nome específico
+3 - Verificar a quantidade de itens
+4 - Sair do programa`));
 
-while(opcao <= 6){
-    
+while( opcao != 3 ){
+    switch(opcao){
+        case 1:
+            nomes.push(nome);
+            break;
+
+        case 2:
+            for(let i=0; i<nomes.length; i++){
+                if(nomes.includes(nome)){
+                    alert(`Nome encontrado no array!`);
+                    break;
+                }
+                else {
+                    alert(`Nome não encontrado!`);
+                    break;
+                }
+            }
+        
+        case 3:
+            alert(nomes.length);
+            break;
+
+        default:
+            break;
+    }
+    console.log(nomes);
+
+    let nome = prompt('Digite um nome: ');
+
+    opcao = parseInt(prompt(`Escolha uma opção a ser realizada:
+    1 - Adicionar um nome
+    2 - Encontrar um nome específico
+    3 - Listar os nomes inseridos
+    4 - Sair do programa`));
+
 }
