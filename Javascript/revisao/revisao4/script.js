a2f8a27e3d9fb23fa9a5b5329ab3cedf6aa12e14
brunosoharagit <b.sohara.git@gmail.com>
2022-05-06 13:25:29 -0300

    desafio front end: alterações de margin e padding dos componentes

--- Javascript/revisao/revisao4/script.js
+++ Javascript/revisao/revisao4/script.js
@@ -1,44 +1,41 @@
 let nomes = [];
-let nome = prompt('Digite um nome: ');
-let opcao = parseInt(prompt(`Escolha uma opção a ser realizada:
-1 - Adicionar um nome
-2 - Encontrar um nome específico
-3 - Verificar a quantidade de itens
-4 - Sair do programa`));
 
-while( opcao != 3 ){
+let opcao = prompt(`Insira qual operação será realizada:
+1 - Inserir nome
+2 - Remover nome específico
+3 - Gerar lista
+4 - Sair do programa
+`);
+
+while(opcao != "4"){
     switch(opcao){
-        case 1:
+        case "1":
+            var nome = prompt('Digite um nome: ');
             nomes.push(nome);
             break;
 
-        case 2:
-            for(let i=0; i<nomes.length; i++){
-                if(nomes.includes(nome)){
-                    alert(`Nome encontrado no array!`);
-                    break;
-                }
-                else {
-                    alert(`Nome não encontrado!`);
-                    break;
-                }
-            }
-        
-        case 3:
-            alert(nomes.length);
+        case "2":
+            var nome = prompt('Insira um nome para remover: ');
+            const posicao = nomes.indexOf(nome);
+            (posicao > -1) ? nomes.splice(posicao, 1) : console.log('Nome não encontrado.');
             break;
 
-        default:
+        case "3":
+            document.write('<h1>Times de Projeto</h1>')
+            for(let i=0; i<nomes.length; i++){
+                document.write(`<b>${nomes[i]}</b><br>`);
+            }
             break;
+            console.log(`Lista gerada. Aperte 4 para visualizar.`);
     }
     console.log(nomes);
 
-    let nome = prompt('Digite um nome: ');
-
-    opcao = parseInt(prompt(`Escolha uma opção a ser realizada:
-    1 - Adicionar um nome
-    2 - Encontrar um nome específico
-    3 - Listar os nomes inseridos
-    4 - Sair do programa`));
-
+    opcao = prompt(`Insira qual operação será realizada:
+    1 - Inserir nome
+    2 - Remover nome específico
+    3 - Gerar lista
+    4 - Sair do programa
+    `);
 }
+
+
