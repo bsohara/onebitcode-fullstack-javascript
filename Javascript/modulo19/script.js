6402591070225230b06ee95e2d678a64245a627a
brunosoharagit <b.sohara.git@gmail.com>
2022-05-23 13:02:18 -0300

    OBC 2022.05.23 - splice and slice

---
+++ Javascript/modulo19/script.js
@@ -0,0 +1,24 @@
+let names = ["Gerson", "Miguel", "Icaro"];
+
+console.log(names);
+
+let splicedNames = names.splice(1, 2, "Demetrio", "Pedro", "Heitor");
+
+console.log(names);
+console.log(splicedNames);
+
+let extractedNames = names.slice(0, 2);
+//pega os valores da lista, de acordo com a posição
+
+console.log(names);
+console.log(extractedNames);
+
+names.forEach((name) => console.log(name));
+
+const sums = (a, b) => { return a + b };
+
+console.log(sums(12, 12));
+
+const pow = (a, b) => { return Math.pow(a, b)}
+
+console.log(pow(12, 2));
