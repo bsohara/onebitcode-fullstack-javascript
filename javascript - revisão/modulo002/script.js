0b153e73fe8d6813d98c8b797b968981cd870120
b.sohara.git <b.sohara.git@gmail.com>
2022-06-08 19:27:02 -0300

    OBC 2022.06.08 >> condicionais

---
+++ javascript - revisão/modulo002/script.js
@@ -0,0 +1,23 @@
+let spaceship = 'Keylog';
+let speed = 80;
+
+
+console.log(speed == 80);
+console.log(speed == 90);
+console.log(speed == "80");
+console.log(speed === "80");
+console.log(speed != "80");
+console.log(speed != 80);
+console.log(speed != 90);
+console.log(speed !== "80");
+console.log(speed <= 120);
+console.log(speed >= 120);
+
+console.log('para a string "spaceship"');
+console.log();
+console.log(spaceship == 'Keylog');
+console.log(spaceship == 'Nexus Mortis');
+console.log(spaceship > 'Ueda');
+console.log(spaceship < 'Dena');
+console.log(spaceship >= 'Keylog');
+console.log(spaceship <= 'Noctis')
