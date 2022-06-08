0f2a067934a610fbb766c50e4b8608fbe0cccaa5
b.sohara.git <b.sohara.git@gmail.com>
2022-06-08 19:13:54 -0300

    OBC 20022.06.08 >> alterações

--- javascript - exercicios/exercicio02/script.js
+++ javascript - exercicios/exercicio02/script.js
@@ -1,24 +1,24 @@
-let maisVelho = {
-    nome: prompt('Qual o nome da pessoa mais velha?'),
-    idade: prompt('Qual a idade dela?')
+let older = {
+    name: prompt('Qual o nome da pessoa mais velha?'),
+    age: prompt('Qual a idade dela?')
 }
 
-let maisNovo = {
-    nome: prompt('Qual o nome da pessoa mais nova?'),
-    idade: prompt('Qual a idade dela?')
+let younger = {
+    name: prompt('Qual o nome da pessoa mais nova?'),
+    age: prompt('Qual a idade dela?')
 }
 
-var calculoDiferenca = (valor1, valor2) => {
-    if(valor1>=18 && valor2>=18){
-        return valor1-valor2;
+const calcDifference = (val1, val2) => {
+    if(val1>=18 && val2>=18){
+        return val1-val2;
     }
     else{
         return null;
     }
 }
 
-alert(`Nome da pessoa mais velha: ${maisVelho.nome}.
-    Idade desta pessoa: ${maisVelho.idade}.
-    Nome da pessoa mais nova: ${maisNovo.nome}.
-    Idade desta pessoa: ${maisNovo.idade}.
-    Diferença das idades: ${calculoDiferenca(maisVelho.idade, maisNovo.idade)} anos.`);
+alert(`Nome da pessoa mais velha: ${older.name}.
+    Idade desta pessoa: ${older.age}.
+    \nNome da pessoa mais nova: ${younger.name}.
+    Idade desta pessoa: ${younger.age}.
+    \nDiferença das idades: ${calcDifference(older.age, younger.age)} anos.`);
