f0ef0d5e76e43c9b03c3003490bd19fc94cd7db1
b.sohara.git <b.sohara.git@gmail.com>
2022-06-11 12:15:05 -0300

    OBC 2022.06.11 >> alterações

--- javascript - exercicios/exercicio05/script.js
+++ javascript - exercicios/exercicio05/script.js
@@ -2,6 +2,7 @@ let espaconave = prompt("Qual o nome da sua nave? ");
 let resposta = prompt("Gostaria de fazer uma dobra?\n1-sim\n2-não");
 let dobras = 0;
 
+//while
 const fazerDobras = (resposta) => {
     while(resposta == "1"){
         dobras++;
@@ -11,6 +12,17 @@ const fazerDobras = (resposta) => {
     
     alert(`Nome da espaçonave: ${espaconave}.\nNúmero de dobras: ${dobras} dobras.`);
 }
+
+//do-while
+/*const fazerDobras = (resposta) => {
+    do {
+        dobras++;
+        console.log(`Dobra nº: ${dobras}`);
+        resposta = prompt("Gostaria de fazer mais uma dobra?\n1-sim\n2-não");
+    } while(resposta == "1")
+    
+    alert(`Nome da espaçonave: ${espaconave}.\nNúmero de dobras: ${dobras} dobras.`);
+}*/
 
 fazerDobras(resposta);
 
