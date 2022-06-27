290b3a574f86d54a6fe375cf7302dae31dcbdb35
b.sohara.git <b.sohara.git@gmail.com>
2022-06-27 19:46:52 -0300

    OBC 2022.06.27 >> alterações

--- javascript - exercicios/exercicio07/script.js
+++ javascript - exercicios/exercicio07/script.js
@@ -2,6 +2,9 @@ var spaceshipName = prompt("Qual o nome da suua espaçonave?")
 var reversedSpaceshipName = "";
 var profaneChar = prompt("Qual letra da espaçonave quer que não apareça?")
 
+//caminho normal: começando da primeira até a última posição da letra
+//caminho invertido: começando pela última letra até a primeira letra
+
 for(let i=spaceshipName.length-1; i>=0; i--){
     if(spaceshipName[i] == profaneChar){
         break;
