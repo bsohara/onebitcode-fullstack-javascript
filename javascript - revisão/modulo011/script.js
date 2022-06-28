6f0271533d6198a98c518b1f9b92830b40977f64
b.sohara.git <b.sohara.git@gmail.com>
2022-06-28 19:06:43 -0300

    OBC 2022.06.28 > alterações e tipos de codificação de funções

---
+++ javascript - revisão/modulo011/script.js
@@ -0,0 +1,50 @@
+/*let nome = "Bruno";
+
+function alterarNome(){
+    nome = "Breno";
+}
+
+console.log(nome);
+
+alterarNome();
+
+console.log(nome);
+
+function startSpaceshipSpeed() {
+    let spaceshipSpeed = 0;
+}
+
+let spaceshipSpeed = 900;
+
+startSpaceshipSpeed();
+
+console.log(spaceshipSpeed);*/
+
+
+/*function setSpaceshipDetails() {
+    let speed = 50;
+    if (speed == 50) {
+        var name = "Cuzeta";
+        let type = "Cheirador de Crack";
+    }
+
+    console.log(speed);
+    console.log(name);
+    console.log(type);
+}
+
+setSpaceshipDetails();*/
+
+
+function setSpaceshipDetails() {
+    console.log(name);
+    console.log(type);
+
+    var name = "Cuzeta";
+    let type = "Cheira pó";
+
+    console.log(name);
+    console.log(type);
+}
+
+setSpaceshipDetails();
