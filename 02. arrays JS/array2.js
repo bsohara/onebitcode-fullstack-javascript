5608f12dffd42c34beabe684f3179c88574a55f7
b.sohara.git <b.sohara.git@gmail.com>
2022-07-08 22:07:34 -0300

    OBC 2022.07.08 > alterações

--- 02. arrays JS/array2.js
+++ 02. arrays JS/array2.js
@@ -1,12 +1,15 @@
 const frutas = ["🍇", "🍎", "🍑", "🍍"];
 
+//alternativa do loop padrão 'for'
+//forma frequentemente utilizada no JavaScript
+frutas.forEach(fruta => {
+    console.log(fruta);
+})
 
 //opção de transformar seus elementos em algo novo, trazendo como resultado um novo array 
+//- facilidade de realizar concatenações de operações, com emenda de outros métodos de arrays
+//- ex: .map().filter
 frutas.map(fruta => {
     console.log(fruta);
 })
 
-//
-frutas.forEach(fruta => {
-    console.log(fruta);
-})
