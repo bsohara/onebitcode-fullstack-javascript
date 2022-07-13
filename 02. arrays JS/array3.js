f22ebe57813610b0db7d72ace6f01de48456f867
b.sohara.git <b.sohara.git@gmail.com>
2022-07-13 18:23:53 -0300

    OBC 2022.07.13

--- 02. arrays JS/array3.js
+++ 02. arrays JS/array3.js
@@ -1,5 +1,6 @@
 let comidas = new Array();
 let comida = '';
+let opcaoEscolhida;
 
 const insere = (lista, valor) => lista.push(valor);
 const inserePrimeiro = (lista, valor) => lista.unshift(valor);
@@ -7,12 +8,17 @@ const inserePrimeiro = (lista, valor) => lista.unshift(valor);
 const removeUltimo = (lista) => lista.pop();
 const removePrimeiro = (lista) => lista.shift();
 
-
 const render = () => {
     comidas = comidas.join(' - ')
     setTimeout(() => {
         alert(`Lista = ${comidas}`);
     }, 4000);
 }
+
+function main() {
+
+}
+
+
 
 //render();
